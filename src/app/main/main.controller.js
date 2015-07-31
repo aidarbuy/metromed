db7d24a07f82ce81ab507bb3720eae0d3246c7bc
aidarbuy <aidarbuy@gmail.com>
2015-07-31 14:54:31 +0600

    Fix layout-sm

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,13 +2,49 @@ class MainController {
 	constructor () {
 		'ngInject';
 
-      this.isOpen = false;
+		// TOP SIDEBAR
+		this.topSidebars = [
+			{
+				header: "CALLING ALL PHYSICALS!",
+				text: [
+					"Come in between July 1, 2015 and August 31, 2015 for any sports or school physical for just $20!",
+					"Looking forward to seeing you here!"
+				]
+			},
+			{
+				header: "Urgent Care",
+				text: [
+					"Our Emergency Medicine doctors can diagnose and treat a wide variety of illnessess and injuries.",
+					"You only need to go to the emergency department to be admitted for extended care or surgery."
+				]
+			},
+			{
+				header: "Self-pay prices",
+				text: [
+					"We guarantee the lowest self-pay prices in the area."
+				]
+			},
+			{
+				header: "Hours of operation",
+				text: [
+					"Open 7 days a week",
+					"from 10am to 8pm.",
+					"We observe major holidays"
+				]
+			}
+		];
 
-      this.demo = {
-        isOpen: false,
-        count: 0,
-        selectedAlignment: 'md-right'
-      };
+		"If You Feel Sick You Should Not Wait",
+		"We are passionate about caring for your health and providing exceptional urgent care"
+
+
+		this.isOpen = false;
+
+		this.demo = {
+			isOpen: false,
+			count: 0,
+			selectedAlignment: 'md-right'
+		};
 	}
 }
 
