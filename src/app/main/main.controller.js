71d7150a1c9f5d990fd20cb900dd92622449ce73
aidarbuy <aidarbuy@gmail.com>
2015-07-31 16:28:02 +0600

    Strict max width on large devices

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,7 +5,7 @@ class MainController {
 		// TOP SIDEBAR
 		this.topSidebars = [
 			{
-				header: "CALLING ALL PHYSICALS!",
+				header: "Calling All Physicals!",
 				text: [
 					"Come in between July 1, 2015 and August 31, 2015 for any sports or school physical for just $20!",
 					"Looking forward to seeing you here!"
@@ -34,8 +34,44 @@ class MainController {
 			}
 		];
 
-		"If You Feel Sick You Should Not Wait",
-		"We are passionate about caring for your health and providing exceptional urgent care"
+		// QUOTE
+		this.quote = {
+			title: "If You Feel Sick You Should Not Wait",
+			text: "We are passionate about caring for your health and providing exceptional urgent care"
+		};
+
+
+		// CUSTOM SERVICES
+		this.customServices = [
+			{
+				icon: "favorite",
+				title: "Cardio Health",
+				text: [
+					"Our clinic is equipped with EKG, X-ray and Laboratory."
+				]
+			},
+			{
+				icon: "local_hospital",
+				title: "Medical Treatment",
+				text: [
+					"We administer breathing treatments, repair lacerations, dispense medications intravenously, remove foreign objects from the eyes, and more."
+				]
+			},
+			{
+				icon: "photo_camera",
+				title: "Photo Gallery",
+				text: [
+					"Check out our pictures!"
+				]
+			},
+			{
+				icon: "accessibility",
+				title: "Aging Clinic",
+				text: [
+					"We organize regular meetings with clients who want to be healthy, youthful and energetic."
+				]
+			},
+		];
 
 
 		this.isOpen = false;
